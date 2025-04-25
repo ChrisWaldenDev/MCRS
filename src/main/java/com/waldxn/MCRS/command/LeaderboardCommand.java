ada2ec5aa1c96bbc1b324572cea56397a795a299
Chriswalden <29243129+Waldxn@users.noreply.github.com>
2025-04-24 20:35:21 -0400

    Added tab-complete for leaderboard command

--- src/main/java/com/waldxn/MCRS/command/LeaderboardCommand.java
+++ src/main/java/com/waldxn/MCRS/command/LeaderboardCommand.java
@@ -8,10 +8,16 @@ import com.waldxn.MCRS.util.ChatUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SuppressWarnings("NullableProblems")
-public class LeaderboardCommand implements CommandExecutor {
+public class LeaderboardCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (!commandSender.hasPermission("mcrs.player.leaderboard")) {
@@ -49,5 +55,16 @@ public class LeaderboardCommand implements CommandExecutor {
         LeaderboardGui.open(PlayerManager.get(player.getUniqueId()), skill);
 
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (strings.length == 1) {
+            return Arrays.stream(SkillType.values())
+                    .map(skill -> skill.name().toLowerCase())
+                    .filter(name -> name.startsWith(strings[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
