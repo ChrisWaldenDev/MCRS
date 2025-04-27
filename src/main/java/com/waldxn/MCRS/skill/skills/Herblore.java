f51d379eb8f505a8a1fe2d796a2dccf0cf25b8db
Waldxn <29243129+Waldxn@users.noreply.github.com>
2025-04-27 16:59:46 -0400

    Added herblore support

--- src/main/java/com/waldxn/MCRS/skill/skills/Herblore.java
+++ src/main/java/com/waldxn/MCRS/skill/skills/Herblore.java
@@ -2,9 +2,22 @@ package com.waldxn.MCRS.skill.skills;
 
 import com.waldxn.MCRS.skill.core.Skill;
 import com.waldxn.MCRS.skill.core.SkillType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Herblore extends Skill {
     public Herblore() {
         super(SkillType.HERBLORE);
+    }
+
+    @Override
+    public List<Component> getHoverInfo() {
+        List<Component> info = new ArrayList<>(super.getHoverInfo());
+        info.add(Component.text("Trained by: Brewing potions in a brewing stand", NamedTextColor.AQUA));
+        info.add(Component.text("Increases herblore mastery", NamedTextColor.DARK_AQUA));
+        return info;
     }
 }
